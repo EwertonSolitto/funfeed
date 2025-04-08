1dee6598226a4ae65f348ba05e3a79429e81574c
Ewerton <ewerton2004@yahoo.com.br>
2025-04-08 14:36:06 -0300

    feat: create component footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
 import { FeedListComponent } from './feed-list/feed-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MenuComponent, FeedListComponent],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent, FeedListComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
