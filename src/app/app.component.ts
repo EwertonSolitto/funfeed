0fe53dbe68bc50f99955918e89302922a38b210f
Ewerton <ewerton2004@yahoo.com.br>
2025-04-07 23:45:50 -0300

    feat: create component feed list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import { FeedListComponent } from './feed-list/feed-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, MenuComponent],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent, FeedListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
