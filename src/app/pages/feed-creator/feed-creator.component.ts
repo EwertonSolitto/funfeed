24ab7e5f9877378099f98f86c6ba0354f33d2586
Ewerton <ewerton2004@yahoo.com.br>
2025-04-11 18:52:24 -0300

    feat: create select for quizes and remove button

--- src/app/pages/feed-creator/feed-creator.component.ts
+++ src/app/pages/feed-creator/feed-creator.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
 import { FooterComponent } from '../../footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-feed-creator',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, MatIconModule],
   templateUrl: './feed-creator.component.html',
   styleUrl: './feed-creator.component.css'
 })
