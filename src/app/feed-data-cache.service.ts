e3bfb19595d7d7f62ec18ca9e9f635fe8c664543
Ewerton <ewerton2004@yahoo.com.br>
2025-05-22 22:45:26 -0300

    fix: fix when get default feed values

--- src/app/feed-data-cache.service.ts
+++ src/app/feed-data-cache.service.ts
@@ -1,44 +1,44 @@
-import { Injectable } from '@angular/core';
-import type { FeedProps } from '../models/FeedProps';
-import defaultFeedList from '../data/defaultFeedList.json'
+import { Injectable } from '@angular/core'
+import type { FeedProps } from '../models/FeedProps'
+import { defaultFeedList } from '../data/defaultFeedList'
 
 @Injectable({
   providedIn: 'root',
 })
 export class FeedDataCacheService {
-  private cache: FeedProps[] = [];
+  private cache: FeedProps[] = []
 
   constructor() {
-    const storedCache = localStorage.getItem('formDataCache');
+    const storedCache = localStorage.getItem('formDataCache')
     if (storedCache) {
       try {
-        this.cache = JSON.parse(storedCache);
+        this.cache = JSON.parse(storedCache)
       } catch (error) {
-        console.error('Erro ao carregar o cache do localStorage:', error);
-        this.cache = [...defaultFeedList.data]
-        this.saveCache();
+        console.error('Erro ao carregar o cache do localStorage:', error)
+        this.cache = [...defaultFeedList]
+        this.saveCache()
       } 
     } else {
-      this.cache = [...defaultFeedList.data]
+      this.cache = [...defaultFeedList]
       this.saveCache()
     }
   }
 
   addFormData(feedData: FeedProps): void {
-    this.cache.push(feedData);
-    this.saveCache();
+    this.cache.push(feedData)
+    this.saveCache()
   }
 
   getAllFormData(): FeedProps[] {
-    return [...this.cache];
+    return [...this.cache]
   }
 
   clearCache(): void {
-    this.cache = []
-    this.saveCache();
+    this.cache = defaultFeedList
+    this.saveCache()
   }
 
   private saveCache(): void {
-    localStorage.setItem('formDataCache', JSON.stringify(this.cache));
+    localStorage.setItem('formDataCache', JSON.stringify(this.cache))
   }
 }
---
+++ src/data/defaultFeedList.ts
@@ -0,0 +1,32 @@
+export const defaultFeedList = [{
+  title: { value: "Teste" },
+  description: { value: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
+  results: [
+    { value: "Teste" },
+    { value: "Teste" }
+  ],
+  quizes: [
+    { title: { value: "Teste"},
+      options: [
+        { value: "Teste", resultSelected: "Teste"},
+        { value: "Teste", resultSelected: "Teste"}
+      ]
+    }
+  ]
+},
+{
+  title: { value: "Teste" },
+  description: { value: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." },
+  results: [
+    { value: "Teste" },
+    { value: "Teste" }
+  ],
+  quizes: [
+    { title: { value: "Teste"},
+      options: [
+        { value: "Teste", resultSelected: "Teste"},
+        { value: "Teste", resultSelected: "Teste"}
+      ]
+    }
+  ]
+}]
