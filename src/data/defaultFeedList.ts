f1fd75d46d8901104f12ccdb56261680d2d9adb0
Ewerton <ewerton2004@yahoo.com.br>
2025-05-29 15:51:07 -0300

    feat: update default feed list to some made with IA

--- src/data/defaultFeedList.ts
+++ src/data/defaultFeedList.ts
@@ -1,31 +1,91 @@
 export const defaultFeedList = [{
-  title: { value: "Teste" },
-  description: { value: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."},
+  title: { value: "Which Meme Are You Today?" },
+  description: { value: "Discover which meme represents your current mood and have fun with the result!" },
   results: [
-    { value: "Teste" },
-    { value: "Teste" }
+    { value: "The Distracted Boyfriend meme: You're always torn between several things, but in the end, it's all about the fun." },
+    { value: "The Doge meme: So much cuteness, much wow, many things happening in your head at the same time." },
+    { value: "The Woman Yelling at Cat meme: You're going through some struggles, but deep down, you just want peace and some good affection (or a whole pizza)." },
+    { value: "The Madagascar Penguin meme: You're strategic, planning every step, but in the end, improvisation saves the day." },
+    { value: "The Coffin Dance meme: You're one of those who face problems with unwavering good humor, turning any situation into a party." }
   ],
   quizes: [
-    { title: { value: "Teste"},
+    {
+      title: { value: "How do you deal with a rainy Monday?" },
       options: [
-        { value: "Teste", resultSelected: "Teste"},
-        { value: "Teste", resultSelected: "Teste"}
+        { value: "I drag myself out of bed, complaining about life.", resultSelected: "The Woman Yelling at Cat meme: You're going through some struggles, but deep down, you just want peace and some good affection (or a whole pizza)." },
+        { value: "I switch to 'Don't Care' mode and focus on coffee.", resultSelected: "The Madagascar Penguin meme: You're strategic, planning every step, but in the end, improvisation saves the day." },
+        { value: "I'm already planning the next holiday.", resultSelected: "The Distracted Boyfriend meme: You're always torn between several things, but in the end, it's all about the fun." },
+        { value: "I sing in the shower and dance in the rain.", resultSelected: "The Coffin Dance meme: You're one of those who face problems with unwavering good humor, turning any situation into a party." }
+      ]
+    },
+    {
+      title: { value: "What's your ideal late-night snack?" },
+      options: [
+        { value: "Something quick and easy, like a cookie.", resultSelected: "The Distracted Boyfriend meme: You're always torn between several things, but in the end, it's all about the fun." },
+        { value: "Whatever's in the fridge, no fuss.", resultSelected: "The Doge meme: So much cuteness, much wow, many things happening in your head at the same time." },
+        { value: "A whole pizza, no guilt.", resultSelected: "The Coffin Dance meme: You're one of those who face problems with unwavering good humor, turning any situation into a party." },
+        { value: "Popcorn and a movie, to extend the night.", resultSelected: "The Madagascar Penguin meme: You're strategic, planning every step, but in the end, improvisation saves the day." }
       ]
     }
   ]
 },
 {
-  title: { value: "Teste" },
-  description: { value: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." },
+  title: { value: "Discover Your Useless (But Hilarious) Superpower!" },
+  description: { value: "Not every superpower needs to save the world. What's your most peculiar and fun talent?" },
   results: [
-    { value: "Teste" },
-    { value: "Teste" }
+    { value: "You can tell the expiration date of any food just by looking. No more unpleasant surprises!" },
+    { value: "You have the power to attract pigeons wherever you go. Get ready to be the center of attention (theirs, at least)." },
+    { value: "You can untangle any headphone knot in record time. Say goodbye to frustration!" },
+    { value: "You have the ability to find the lost sock from any laundry pile. A true closet hero!" },
+    { value: "You always pick the slowest line at the supermarket. Is it a gift, or a curse? Who knows!" }
   ],
   quizes: [
-    { title: { value: "Teste"},
+    {
+      title: { value: "What's your greatest life achievement?" },
       options: [
-        { value: "Teste", resultSelected: "Teste"},
-        { value: "Teste", resultSelected: "Teste"}
+        { value: "Surviving Monday.", resultSelected: "You have the ability to find the lost sock from any laundry pile. A true closet hero!" },
+        { value: "Getting 8 hours of sleep straight.", resultSelected: "You can untangle any headphone knot in record time. Say goodbye to frustration!" },
+        { value: "Making instant noodles without burning them.", resultSelected: "You can tell the expiration date of any food just by looking. No more unpleasant surprises!" },
+        { value: "Paying all bills on time.", resultSelected: "You always pick the slowest line at the supermarket. Is it a gift, or a curse? Who knows!" }
+      ]
+    },
+    {
+      title: { value: "What's your strategy to avoid annoying people?" },
+      options: [
+        { value: "Pretend you didn't hear them.", resultSelected: "You can tell the expiration date of any food just by looking. No more unpleasant surprises!" },
+        { value: "Invent a flimsy excuse.", resultSelected: "You have the ability to find the lost sock from any laundry pile. A true closet hero!" },
+        { value: "Put on headphones and ignore them.", resultSelected: "You can untangle any headphone knot in record time. Say goodbye to frustration!" },
+        { value: "Attract pigeons to distract them.", resultSelected: "You have the power to attract pigeons wherever you go. Get ready to be the center of attention (theirs, at least)." }
+      ]
+    }
+  ]
+},
+{
+  title: { value: "Is Your Lifestyle More 'Netflix & Chill' or 'Club & Tequila'?" },
+  description: { value: "Find out if you're on the relaxed team or the one that never misses a party!" },
+  results: [
+    { value: "You're the master of 'Netflix & Chill': Comfort and series binges are your thing. Pajamas are the new formal wear!" },
+    { value: "You're the queen of 'Club & Tequila': Your energy is contagious, and the dance floor is your natural habitat. Let the party not stop!" },
+    { value: "You're a perfect hybrid: You love a good party, but you also enjoy a lazy day. Balance is key!" },
+    { value: "You're the unexpected adventurer: Suddenly, you're in the middle of a trail or trying out an extreme sport. Life is a box of surprises!" }
+  ],
+  quizes: [
+    {
+      title: { value: "What do you do on a Saturday night?" },
+      options: [
+        { value: "Order delivery and start a new series.", resultSelected: "You're the master of 'Netflix & Chill': Comfort and series binges are your thing. Pajamas are the new formal wear!" },
+        { value: "Call the gang to go clubbing.", resultSelected: "You're the queen of 'Club & Tequila': Your energy is contagious, and the dance floor is your natural habitat. Let the party not stop!" },
+        { value: "Depends on my mood, I might do both!", resultSelected: "You're a perfect hybrid: You love a good party, but you also enjoy a lazy day. Balance is key!" },
+        { value: "Look for something unusual to do, like an escape room.", resultSelected: "You're the unexpected adventurer: Suddenly, you're in the middle of a trail or trying out an extreme sport. Life is a box of surprises!" }
+      ]
+    },
+    {
+      title: { value: "What's your favorite drink?" },
+      options: [
+        { value: "Hot chocolate.", resultSelected: "You're the master of 'Netflix & Chill': Comfort and series binges are your thing. Pajamas are the new formal wear!" },
+        { value: "Craft beer.", resultSelected: "You're a perfect hybrid: You love a good party, but you also enjoy a lazy day. Balance is key!" },
+        { value: "Colorful cocktails.", resultSelected: "You're the queen of 'Club & Tequila': Your energy is contagious, and the dance floor is your natural habitat. Let the party not stop!" },
+        { value: "Coconut water on the beach.", resultSelected: "You're the unexpected adventurer: Suddenly, you're in the middle of a trail or trying out an extreme sport. Life is a box of surprises!" }
       ]
     }
   ]
